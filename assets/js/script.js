3870e333f8afcb025810177e6922a5e75eb8689a
obrad97 <janiobrad@gmail.com>
2021-10-23 20:44:15 +0200

    almost finished, need to fix a few things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+const player = document.querySelector('.player');
 const video = document.querySelector('.video');
+const videoControlls = document.querySelector('.video-controlls');
 const progressBar = document.querySelector('.progress-bar');
 const currentTime = document.getElementById('current-time');
 const videoDuration = document.getElementById('duration');
@@ -9,9 +11,38 @@ const muteOn = document.querySelector('.mute');
 const muteOff = document.querySelector('.sound-on');
 const fullScreen = document.querySelector('.fullscreen');
 const smallScreen = document.querySelector('.smallscreen');
+const playBackSpeed = document.querySelector('.playback-speed');
+const playBackModal = document.querySelector('.playback-speed-modal');
+const speedRates = document.querySelectorAll('.speed-rate');
 var currentVolume = 0;
 var mouseIsDown = false;
+var fullScreenIsOn = false;
+var timer;
 
+video.onloadedmetadata = function () {
+    defaultVolumeSet();
+    let seconds = Math.floor(video.duration);
+    let minutes = seconds / 60;
+    videoDuration.innerText = (minutes < 1) ?  `0:${seconds}` : `${minutes}:${seconds}`;
+    currentTime.innerText =  '0:00';
+    progressBar.max = seconds;
+    video.playbackRate = 1;
+    playBackSpeed.innerText = "1.0x";
+    document.querySelector('.speed-rate[data-value="1"]').classList.add('active-speed');
+}
+
+video.addEventListener('mousemove',(e)=> {
+    videoControlls.style.bottom = '0px';
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+        videoControlls.style.bottom = '-60px';
+    }, 1500);
+})
+
+videoControlls.addEventListener('mouseover', (e)=>{
+    clearTimeout(timer);
+    videoControlls.style.bottom = '0px'
+}) 
 
 const defaultVolumeSet = () => {
     let volumeProgress = (volumeBar.value-volumeBar.min)/(volumeBar.max-volumeBar.min)*100;
@@ -37,7 +68,10 @@ const timeUpdateHandler =()=> {
         currentSeconds = seconds;
     }
     currentTime.innerText = minutes +':'+currentSeconds
-
+    if (video.currentTime == video.duration){
+        playVid.classList.toggle('active');
+        pauseVid.classList.toggle('active');
+    }
 }
 
 const playPauseVid = ()=> {
@@ -52,7 +86,7 @@ const playPauseVid = ()=> {
     }
 }
 
-const setVolume =  () =>{
+const setVolume =  ()=>{
     video.volume = volumeBar.value / 100;
     let progress = (volumeBar.value-volumeBar.min)/(volumeBar.max-volumeBar.min)*100;
     volumeBar.style.background =  `linear-gradient(to right, white 0%, white ${progress}%, rgba(0, 0 , 0 , 0.3) ${progress}%, rgba(0, 0 , 0 , 0.3) 100%)`;
@@ -81,6 +115,59 @@ const muteUnmute = ()=> {
     setVolume();
 }
 
+const openFullScreen = ()=> {
+    if (player.requestFullscreen) {
+        player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) { 
+        player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) { 
+        player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) { 
+        player = window.top.document.body; 
+        player.msRequestFullscreen();
+    }
+    fullScreen.classList.toggle('active');
+    smallScreen.classList.toggle('active');
+    fullScreenIsOn = true;
+}
+
+const closeFullScreen = ()=> {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        window.top.document.msExitFullscreen();
+    }
+    fullScreen.classList.toggle('active');
+    smallScreen.classList.toggle('active');
+    fullScreenIsOn = false;
+}
+
+const openClosePlayback =()=> {
+    playBackModal.classList.toggle('playback-speed-modal-active');
+}
+
+const playBackSpeedChange = (speedRate)=> {
+    let activeSpeed = document.querySelector('.speed-rate.active-speed');
+        if (activeSpeed && activeSpeed !== speedRate){
+            activeSpeed.classList.remove('active-speed');
+        }
+        speedRate.classList.toggle('active-speed');
+        playBackSpeed.innerText = speedRate.children[0].innerText;
+        video.playbackRate = Number(speedRate.getAttribute('data-value'));
+}
+
+video.addEventListener('dblclick', (e)=> {
+    if(fullScreenIsOn){
+        closeFullScreen();
+    }else{
+        openFullScreen();
+    }
+})
+
 video.addEventListener('timeupdate', timeUpdateHandler);
 video.addEventListener('click', playPauseVid);
 volumeBar.addEventListener('change', setVolume);
@@ -92,13 +179,12 @@ pauseVid.addEventListener('click', playPauseVid);
 progressBar.addEventListener('change',progressBarChange);
 progressBar.addEventListener('mousedown', (e)=> {video.pause()});
 progressBar.addEventListener('mouseup', (e)=> { video.play()});
-
-video.onloadedmetadata = function () {
-    defaultVolumeSet();
-    let seconds = Math.floor(video.duration);
-    let minutes = seconds / 60;
-    videoDuration.innerText = (minutes < 1) ?  `0:${seconds}` : `${minutes}:${seconds}`;
-    currentTime.innerText =  '0:00';
-    progressBar.max = seconds;
-}
+playBackSpeed.addEventListener('click', openClosePlayback);
+fullScreen.addEventListener('click', openFullScreen);
+smallScreen.addEventListener('click', closeFullScreen);
+speedRates.forEach(speedRate => {
+    speedRate.addEventListener('click', (e)=> {
+        playBackSpeedChange(speedRate)
+    })
+});
 
